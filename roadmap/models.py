656a717955cb542ede4864b312d0c8993e0b39ce
Michał Kozłowski <michal@saturdaysheroes.dev>
2023-05-22 01:25:07 +0200

    chore: edit models

--- roadmap/models.py
+++ roadmap/models.py
@@ -10,29 +10,19 @@ class Repository(models.Model):
     def __str__(self):
         return self.name
 
-class Roadmap(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    repository = models.ForeignKey(Repository, on_delete=models.CASCADE)
-    # Add other fields as needed
-
-    def __str__(self):
-        return f"Roadmap for {self.repository.name}"
-
 class Milestone(models.Model):
-    roadmap = models.ForeignKey(Roadmap, on_delete=models.CASCADE)
+    repository = models.ForeignKey(Repository, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     description = models.TextField()
-    due_date = models.DateField()
-    # Add other fields as needed
 
     def __str__(self):
         return self.name
 
 class Task(models.Model):
-    roadmap = models.ForeignKey(Roadmap, on_delete=models.CASCADE)
+    repository = models.ForeignKey(Repository, on_delete=models.CASCADE)
     milestone = models.ForeignKey(Milestone, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=255)
-    isdone = models.BooleanField()
+    isdone = models.BooleanField(default = False)
     # Add other fields as needed
 
     def __str__(self):
